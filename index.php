651a49e5396ed4a87a670475951676c32bc442b5
jpdh1988 <54423825+jpdh88@users.noreply.github.com>
2020-12-03 14:21:40 -0500

    Converts layout to bootstrap

--- index.php
+++ index.php
@@ -7,119 +7,76 @@
         <title>TODO supply a title</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./css/main.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <style>
-            body {
-                display: auto;
+            html, body {
+                height: 100%;
             }
-            main {
-                padding: 0;
-
-                border-radius: 1em;
-
-                display: grid;
-                grid-template-columns: 1fr 1fr;
-                align-items: center;
+            @media screen and (min-width: 992px) { /* lg and above */
+                .jh-height {
+                    height: 100vh;
+                }
+                #image {
+                    background-position: right;
+                    background-size: auto 100%;
+                }
             }
-                section.figure {
-                    position: relative;
-                    width: 100%;
-                    margin-bottom: 0;
+            @media screen and (max-width: 991px) { /* md and below */
+                .jh-height {
+                    height: 50vh;
                 }
-                        section.figure:hover {
-                            animation: anim-filter infinite alternate 3s;
-                        }
-                            @keyframes anim-filter {
-                                0% {
-                                    filter: hue-rotate(0deg);
-                                }
-                                100% {
-                                    filter: hue-rotate(360deg)
-                                }
-                            }
-                        section.figure:hover > aside {
-                            animation: anim-font-size 6s forwards;
-                        }
-                        section.figure:hover > img {
-                            opacity: 1;
-                        }
-                    section.figure > img {
-                            width: 100%;
-                            margin-bottom: 0;
-
-                            opacity: 0.3;
-                            transition: opacity 1s;
-
-                            border-top-left-radius: 1em;
-                            border-bottom-left-radius: 1em;
-
-                            display: block;
-                        }
-                        section.figure > aside {
-                            position: absolute;
-                            top: 25px;
-                            left: 0.5em;
-
-                            white-space: nowrap;
-                            overflow: hidden;
-
-                            color: white;
-                            font-size: 12pt;
-                            /* transition: 6s font-size; */
-                        }
-                            @keyframes anim-font-size {
-                                0% {
-                                    font-size: 12pt;
-                                }
-                                100% {
-                                    font-size: 50pt;
-                                }
-                            }
-                section.content {
-                    width: 100%;
-                    padding: 1em;
-
-                    font-size: medium;
+                #image {
+                    background-size: 100% auto;
+                    background-position: right center;
                 }
-                    section.content > p {
-                        margin-left: 3.5em;
-                        margin-bottom: 0.5em;
-                    }
-                    section.content > p:nth-of-type(1) {
-                            opacity: 0.2;
-                        }
-                        section.content > p:nth-of-type(3) {
-                            text-align: right;
-                            transform: rotate(180deg);
-                        }
-                        section.content > p:last-of-type {
-                            padding-left: 1em;
-                        }
-                    a.button {
-                        display: block;
-                        margin-bottom: 0.5em;
-
-                        border-radius: 1em;
-                    }
+            }
+            @media screen and (max-width: 719px) { /* to fix background image */
+                #image {
+                    background-position: right;
+                    background-size: auto 100%;
+                }
+            }
+            #image {
+                background-image: url("./images/monkey-mirror.jpg");
+            }
+            p:first-of-type {
+                margin-left: 2em;
+            }
+            p:nth-of-type(2) {
+                margin-left: 3em;
+            }
+            p:nth-of-type(3) {
+                margin-left: 4em;
+            }
+            p:nth-of-type(4) {
+                margin-left: 5em;
+            }
+            p:nth-of-type(5) {
+                margin-left: 6em;
+            }
         </style>
     </head>
     <body>
-        <div class="container">
-            <main>
-                <section class="figure">
-                    <img src="./images/monkey-mirror.jpg">
-                    <aside>iProjector - a new way to look at yourself!</aside>
-                </section>
-                <section class="content">
-                    <p>Record your thoughts.</p>
-                    <p>See other peoples' thoughts.</p>
-                    <p>Shuffle them around.</p>
-                    <p>We do the hokey pokey</p>
-                    <p>and that's what it's all about.</p>
-                    <a href="./log_in.php" class="button">Log in</a>
-                    <a href="./create_account.php" class="button">Create account</a>
-                </section>
-            </main>
+        <div class="container-fluid jh-height" id="container">
+            <div class="row">
+                <div class="col-lg jh-height bg-primary" id="image">
+                </div>
+                <div class="col-lg jh-height row justify-content-center align-items-center p-0 m-0" id="content">
+                    <div class="">
+                        <h1 class="font-weight-bold">iProjector</h1>
+                        <h2>a new way to look at yourself</h2>
+                        <p>Record your thoughts.</p>
+                        <p>See other peoples' thoughts.</p>
+                        <p>Shuffle them around.</p>
+                        <p>We do the hokey pokey</p>
+                        <p>and that's what it's all about.</p>
+                        <div class="row">
+                            <a href="./log_in.php" class="col p-2 text-center rounded-pill mr-1 bg-secondary text-white">Log in</a>
+                            <a href="./create_account.php" class=" col p-2 text-center rounded-pill bg-primary text-white">Create account</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 </html>
