4700deb6f7eea2f83909d5bf0a63756dc1c3361c
jpdh1988 <54423825+jpdh88@users.noreply.github.com>
2020-12-10 19:48:12 -0500

    Adds ability to get a random thought from db

--- php_scripts/get_one_thought_from_db.php
+++ php_scripts/get_one_thought_from_db.php
@@ -12,12 +12,24 @@
 
     $output = [];
     $user_id = get("user_id");
-    $thought_id = filter_input(INPUT_GET, "thought_id", FILTER_VALIDATE_INT);
+    $thought_id = filter_input(INPUT_GET, "thought_id", FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if ($thought_id != "random") {
+        $thought_id = filter_input(INPUT_GET, "thought_id", FILTER_VALIDATE_INT);
+    }
     
     if (userIsLoggedIn() and $thought_id != null and $thought_id != false) {
-        $command = "SELECT t.id as thought_id, t.current_owner, t.date_created, t.thought_text, t.huzzahs, u.id as user_id, u.username, u.age FROM thoughts t INNER JOIN users u ON t.current_owner=u.id WHERE t.id = ?";
-        $statement = $dbh->prepare($command);
-        $was_successful = $statement->execute([$thought_id]);
+        $was_successful = "";
+
+        if ($thought_id == "random") {
+            $command = "SELECT t.id as thought_id, t.current_owner, t.date_created, t.thought_text, t.huzzahs, u.id as user_id, u.username, u.age FROM thoughts t INNER JOIN users u ON t.current_owner=u.id ORDER BY RAND() LIMIT 1";
+            $statement = $dbh->prepare($command);
+            $was_successful = $statement->execute([]);
+        } else {
+            $command = "SELECT t.id as thought_id, t.current_owner, t.date_created, t.thought_text, t.huzzahs, u.id as user_id, u.username, u.age FROM thoughts t INNER JOIN users u ON t.current_owner=u.id WHERE t.id = ?";
+            $statement = $dbh->prepare($command);
+            $was_successful = $statement->execute([$thought_id]);
+        }
 
         if ($was_successful) {
             $output = $statement->fetchAll();
